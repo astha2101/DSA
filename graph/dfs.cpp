2177f21b8ebbb2de1f03669cbbe2254ab7551652
astha2101 <64977854+astha2101@users.noreply.github.com>
2022-05-24 23:26:10 +0530

    Create dfs.cpp

---
+++ graph/dfs.cpp
@@ -0,0 +1,30 @@
+vector<bool> visited(V, false);
+ 
+    // Create a stack for DFS
+    stack<int> stack;
+ 
+    // Push the current source node.
+    stack.push(s);
+ 
+    while (!stack.empty())
+    {
+        // Pop a vertex from stack and print it
+        int s = stack.top();
+        stack.pop();
+ 
+        // Stack may contain same vertex twice. So
+        // we need to print the popped item only
+        // if it is not visited.
+        if (!visited[s])
+        {
+            cout << s << " ";
+            visited[s] = true;
+        }
+ 
+        // Get all adjacent vertices of the popped vertex s
+        // If a adjacent has not been visited, then push it
+        // to the stack.
+        for (auto i = adj[s].begin(); i != adj[s].end(); ++i)
+            if (!visited[*i])
+                stack.push(*i);
+    }
